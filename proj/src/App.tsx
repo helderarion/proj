b4b88e4452d108ed338a3181635afd988d53dad5
helderarion <164091816+helderarion@users.noreply.github.com>
2024-10-29 00:21:26 +0000

    roteiro parte 1.2

--- proj/src/App.tsx
+++ proj/src/App.tsx
@@ -3,6 +3,7 @@ import Logo from "./components/Logo";
 import Navegacao from "./components/Navegacao";
 import Topo from "./components/Topo";
 import Principal from "./components/Principal"; 
+import styles from "./App.module.css"
 
 const App = () => {
   return (
